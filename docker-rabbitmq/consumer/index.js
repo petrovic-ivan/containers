400cf0e7b5b4def0d6ea577a213ef704b21df2ef
Petrovic, Ivan <ivan.petrovic@moodys.com>
2020-02-13 12:16:42 -0800

    Added some changes.

--- docker-rabbitmq/consumer/index.js
+++ docker-rabbitmq/consumer/index.js
@@ -1,26 +1,27 @@
 const amqp = require('amqplib/callback_api');
 
-amqp.connect('amqp://localhost:5672', function(error0, connection) {
-    if (error0) {
-        throw error0;
-    }
-    connection.createChannel(function(error1, channel) {
-        if (error1) {
-            throw error1;
-        }
+setTimeout(() => {
 
-        const queue = 'node-app';
+    amqp.connect('amqp://rabbitmq:5672', function (error0, connection) {
+        if (error0) throw error0;
 
-        channel.assertQueue(queue, {
-            durable: false
-        });
+        connection.createChannel(function (error1, channel) {
+            if (error1) throw error1;
 
-        console.log(" [*] Consumer waiting for messages in queue '%s'. To exit press CTRL+C", queue);
+            const queue = 'node-app';
 
-        channel.consume(queue, function(msg) {
-            console.log(" [x] Received %s", msg.content.toString());
-        }, {
-            noAck: true
+            channel.assertQueue(queue, {
+                durable: false
+            });
+
+            console.log(" [*] Consumer waiting for messages in queue '%s'. To exit press CTRL+C", queue);
+
+            channel.consume(queue, function (msg) {
+                console.log(" [x] Received %s", msg.content.toString());
+            }, {
+                noAck: true
+            });
         });
     });
-});
+
+}, 5000);
--- docker-rabbitmq/publisher/src/controllers/message.controller.js
+++ docker-rabbitmq/publisher/src/controllers/message.controller.js
@@ -1,11 +1,11 @@
 const amqp = require('amqplib/callback_api');
 
 async function publish(req, res) {
-  if (!req.body.message) res.send(415);
+  if (!req.body.message) res.sendStatus(415);
 
   const data = '<' + new Date().toLocaleString() + '> ' + req.body.message;
 
-  amqp.connect('amqp://localhost:5672', function (error0, connection) {
+  amqp.connect('amqp://rabbitmq:5672', function (error0, connection) {
     if (!error0) {
       connection.createChannel(function (error1, channel) {
         if (!error1) {
@@ -17,17 +17,12 @@ async function publish(req, res) {
           });
           channel.sendToQueue(queue, Buffer.from(msg));
 
-          res.send(200);
+          res.sendStatus(200);
         }
       });
-
     } else {
-      res.send(500);
+      res.sendStatus(500);
     }
-
-    setTimeout(function () {
-      connection.close();
-    }, 2000);
   });
 
 
